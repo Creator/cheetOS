b2a80b8fb60ccb4c3ddb809f2b91a250c115d018
GeorgeMcKinlay <grgmckinlay@gmail.com>
2015-08-05 14:28:22 +0200

    Shell: Fixed not being able to access files from other drive

--- system/sysinit.lua
+++ system/sysinit.lua
@@ -14,13 +14,13 @@ local ok, err = pcall(function()
 		"R:/programs/pocket";
 		"R:/programs/command";
 	}
-	
+
 	local aliases = {}
-	
+
 	local associations = {
 		["ss"] = "sshost.lua"
 	}
-	
+
 	local shell = {} 	-- Shell API
 
 	shell.exit = function(force)
@@ -36,7 +36,7 @@ local ok, err = pcall(function()
 		if not fs.exists(path) then
 			error("invalid path!", 2)
 		end
-		
+
 		cdir = path
 	end
 
@@ -45,7 +45,7 @@ local ok, err = pcall(function()
 		for arg in file:gmatch("[^%s]+") do
 			args[#args + 1] = arg
 		end
-		
+
 		local varargs = { ... }
 		for _,v in pairs(varargs) do
 			args[#args + 1] = v
@@ -54,14 +54,14 @@ local ok, err = pcall(function()
 		if args[1] == nil then
 			return false
 		end
-		
+
 		file = shell.resolveProgram(args[1])
-	
+
 		if not fs.exists(file) then
 			printError("No such program '" .. file .. "'")
 			return false
 		end
-		
+
 		local ext = fs.getName(file):match(".+%.(.+)")
 		if ext ~= nil then
 			local association = shell.__getAssociatedProgram(ext)
@@ -69,78 +69,90 @@ local ok, err = pcall(function()
 				association = shell.resolveProgram(association)
 				local prog = System.Tasks.NewTask(association)
 				prog:Start(file)
-				
+
 				System.Tasks.WaitForTask(prog)
 				return true
 			end
 		end
-		
+
 		local prog = System.Tasks.NewTask(file)
 		prog:Start(select(2, unpack(args)))
-		
+
 		System.Tasks.WaitForTask(prog)
 		return true
 	end
-	
+
 	shell.path = function()
 		error("shell.path(): not available in basic shell!", 2)
 	end
-	
+
 	shell.setPath = function(newPath)
 		error("shell.path(): not available in basic shell!", 2)
 	end
-	
+
 	shell.__addToPath = function(dir)
 		path[#path + 1] = dir
 	end
-	
+
 	shell.__removeFromPath = function(dir)
 		local index = 0
-		
+
 		for i,v in pairs(path) do
 			if System.Path.Normalise(v) == System.Path.Normalise(dir) then
 				index = i
 				break
 			end
 		end
-		
+
 		table.remove(path, index)
 	end
-	
+
 	shell.setAlias = function(alias, prog)
 		aliases[alias] = prog
 	end
-	
+
 	shell.aliases = function()
 		return aliases
 	end
-	
+
 	shell.clearAlias = function(alias)
 		aliases[alias] = nil
 	end
-	
+
 	shell.__resolveAlias = function(alias)
 		return aliases[alias]
 	end
-	
+
 	shell.programs = function()
 		return {}
 	end
-	
+
+	shell.__drive = function()
+		return System.Path.GetDriveAndPath(cdir)
+	end
+
 	shell.resolve = function(file)
+		local drive,_ = System.Path.GetDriveAndPath(file)
+		local cdrive = shell.__drive()
+		local _cdir = cdir
+
+		if drive ~= nil and cdrive ~= drive then
+			_cdir = drive .. ":/"
+		end
+
 		local firstChar = file:sub(1, 1)
 		if firstChar == "/" or firstChar == "\\" then -- root!
 			return System.Path.Combine("", file)
 		else
-			return System.Path.Combine(cdir, file)
+			return System.Path.Combine(_cdir, file)
 		end
 	end
-	
+
 	shell.resolveProgram = function(name)
 		if aliases[name] ~= nil then
 			name = aliases[name]
 		end
-		
+
 		for _,entry in pairs(path) do
 			if fs.exists(entry) and fs.isDir(entry) then
 				for _, file in pairs(fs.list(entry)) do
@@ -150,10 +162,10 @@ local ok, err = pcall(function()
 				end
 			end
 		end
-		
+
 		return System.Path.Normalise(shell.resolve(name))
 	end
-	
+
 	--[[shell.getRunningProgram = function()
 		if getfenv then
 			return getfenv(2).__FILE__ or "N/A"
@@ -161,24 +173,24 @@ local ok, err = pcall(function()
 			error("shell.getRunningProgram not available in Lua 5.2!", 2)
 		end
 	end]]
-	
+
 	shell.__associate = function(ext, program)
 		associations[ext] = program
 	end
-	
+
 	shell.__clearAssociation = function(ext)
 		associations[ext] = nil
 	end
-	
+
 	shell.__getAssociatedProgram = function(ext)
 		return associations[ext]
 	end
 
 	System.ShellMgr.SetShell(shell)
-	
+
 	local sh = System.Tasks.NewTask("S:/shell.lua")
 	sh:Start()
-	
+
 	shell.run("U:/start.ss")
 end)
 
