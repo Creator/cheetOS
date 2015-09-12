4d0c4592b5df6e4c0ee660ce6d6e0d155b34c6c4
GeorgeMcKinlay <grgmckinlay@gmail.com>
2015-09-13 01:10:59 +0200

    Fixed devservice.lua crash

--- system/default/devservice.lua
+++ system/default/devservice.lua
@@ -1,8 +1,8 @@
 while true do
   local e, side = os.pullEvent("peripheral", "peripheral_detach")
   if e == "peripheral" then
-    System.Devices.Register(side, peripheral.wrap(side))
+    System.Network.Register(side, peripheral.wrap(side))
   elseif e == "peripheral_detach" then
-    System.Devices.Unregister(side)
+    System.Network.Unregister(side)
   end
 end
